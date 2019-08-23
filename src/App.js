4a8eeff2f51c217bee12ef85d3c25e54e3c4b9bd
Shivam Kukreja <skukreja@sapient.com>
2019-08-23 17:53:43 +0530

    Sass-Repsonsive-Grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div class="container">
+        <div class="container__row">
+          <div class="container__col-12 hidden-sm-down">hidden below 576</div>
+        </div>
+        <div class="container__row">
+          <div class="container__col-6">col-6</div>
+          <div class="container__col-6 hidden">hidden always</div>
+        </div>
+        <div class="container__row">
+          <div class="container__col-4 container__col-lg-6 container__col-lg-offset-2">col-4 col-lg-6 col-lg-offset-2</div>
+          <div class="container__col-4 container__col-lg-6 container__col-lg-offset-2">col-4 col-lg-6 col-lg-offset-2</div>
+          <div class="container__col-4 hidden-lg-up">col-4 hidden in lg</div>
+        </div>
+        <div class="container__row">
+          <div class="container__col-12 container__col-md-3">col-md-3 col-12</div>
+          <div class="container__col-12 container__col-md-3">col-md-3 col-12</div>
+          <div class="container__col-12 container__col-md-3 hidden-lg-up">hidden in 991</div>
+          <div class="container__col-12 container__col-md-3">col-md-3 col-12</div>
+        </div>
+        <div class="container__row">
+          <div class="container__col-2">col-2</div>
+          <div class="container__col-2">col-2</div>
+          <div class="container__col-2">col-2</div>
+          <div class="container__col-2">col-2</div>
+          <div class="container__col-2">col-2</div>
+          <div class="container__col-2">col-2</div>
+        </div>
+        <div class="container__row">
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-1">col-1</div>
+        </div>
+        <div class="container__row">
+          <div class="container__col-1">col-1</div>
+          <div class="container__col-2">col-2</div>
+          <div class="container__col-6">col-6</div>
+          <div class="container__col-2">col-2</div>
+          <div class="container__col-1">col-1</div>
+        </div>
+      </div>
     </div>
   );
 }
